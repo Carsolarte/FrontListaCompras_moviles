50fcc2cefe77bb140f509b8f6b4ccff7be6d5b48
Carsolarte <carsolarte@unicauca.edu.co>
2024-06-11 16:20:24 -0500

    Lista de productos y agregar productos prueba

--- src/app/components/agregar-pruducto/agregar-pruducto.component.ts
+++ src/app/components/agregar-pruducto/agregar-pruducto.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-agregar-pruducto',
   templateUrl: './agregar-pruducto.component.html',
   styleUrls: ['./agregar-pruducto.component.css']
 })
 export class AgregarPruductoComponent {
-
+  constructor(private objRouter:Router){}
+  irAgregarSitio(){
+    this.objRouter.navigate(['/agregarSitio']);
+  }
 }
